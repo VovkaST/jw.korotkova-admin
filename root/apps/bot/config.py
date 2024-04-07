93e77eb4a8915b86936d3f7bb52ae120c25a3034
Korotkov Vladimir <vovka.steel911@mail.ru>
2024-04-07 18:26:35 +0300

    #1 Config optimized

--- root/apps/bot/config.py
+++ root/apps/bot/config.py
@@ -1,17 +1,12 @@
 import logging
 
-from django.utils.translation import gettext_lazy as _
-
 from root.contrib.config import AppConfig
 
 
 class BotConfig(AppConfig):
     PREFIX = "BOT"
 
-    VERSION = "1.2"
     TOKEN: str
-    # "Чтобы активировать магию✨, скорее жми /start 🪄."
-    DESCRIPTION: str = _("To activate the magic✨, quickly press /start 🪄.")
     REDIS_URL: str = "redis://localhost:6379/0"
     STATE_STORAGE_PREFIX = "jw_"
     LOGGING_LEVEL = logging.DEBUG
