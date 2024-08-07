613cfaddcdaadffd5a0166ee694625cda9a6c35c
Korotkov Vladimir <vovka.steel911@mail.ru>
2024-08-08 01:24:47 +0300

    #5 Add translation and gettext fixes

--- root/apps/clients/models.py
+++ root/apps/clients/models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import Q
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
 from root.base.models import TimedModel
