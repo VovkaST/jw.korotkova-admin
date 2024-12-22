dd9ea12022b88563a29565e142ea4cfa3e1cd6a7
Korotkov Vladimir <vovka.steel911@mail.ru>
2024-12-22 14:22:13 +0300

    #7 Celery run entrypoint

---
+++ compose/celery/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+NORMAL="\033[0m"
+
+echo -e "${YELLOW}Running Celery beat${NORMAL}"
+
+source ./compose/build.sh
+source ./compose/migrations.sh
+source ./compose/make_messages.sh
+
+echo -e "${GREEN}Run Celery...${NORMAL}"
+celery -A root.tasks worker -B --loglevel=INFO
+echo -e "${GREEN}Celery was stopped${NORMAL}"
