f5acbaabe864df28c18895a6c5a8a7a97edd268d
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-01-11 15:08:57 +0300

    #15: Admin panel translations

--- root/apps/administration/admin.py
+++ root/apps/administration/admin.py
@@ -1,8 +1,17 @@
 from django.conf import settings
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 
 class CoreAdminSite(admin.AdminSite):
-    site_title = settings.TM_LABEL_TEXT + " site admin"
-    site_header = "Administrative panel " + settings.TM_LABEL_TEXT
-    index_title = settings.TM_LABEL_TEXT + " site administration"
+    @property
+    def site_title(self) -> str:
+        return _("%s site admin") % settings.TM_LABEL_TEXT
+
+    @property
+    def site_header(self) -> str:
+        return _("Administrative panel %s") % settings.TM_LABEL_TEXT
+
+    @property
+    def index_title(self) -> str:
+        return _("%s site administration") % settings.TM_LABEL_TEXT
