a951bd63819fcf5e835e44fe933c7deb1e2f6fc4
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-01-25 15:17:52 +0300

    #27: Fix error of None-value of name_or_url argument in telegram_url tag

--- root/core/templatetags/telegram_utils.py
+++ root/core/templatetags/telegram_utils.py
@@ -9,15 +9,20 @@ def telegram_url(name_or_url: str, to_full: bool = True) -> str:
     """
     Tag for convert telegram url to short or full in depends of given argument `to_full`.
     """
+    if not name_or_url:
+        return ""
+
     if name_or_url.startswith("@"):
         if not to_full:
             return name_or_url
         name = name_or_url[1:]
         return settings.TELEGRAM_URL_TEMPLATE.format(name=name)
+
     elif name_or_url.startswith(settings.TELEGRAM_URL):
         if not to_full:
             _url = name_or_url.removeprefix(f"{settings.TELEGRAM_URL}/")
             name = _url.split("/")[0]
             return f"@{name}"
         return name_or_url
+
     return f"@{name_or_url}"
