7e215da53a47985f6690c71fbc22325599e3a8ef
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-01-26 22:14:34 +0300

    #17: Product category field added to admin

--- root/apps/products/admin.py
+++ root/apps/products/admin.py
@@ -51,12 +51,12 @@ class ProductAdmin(admin.ModelAdmin):
 
         get_message_link.short_description = _("Message link")
 
-    list_display = ["get_lot", "get_type_name", "title", "price", "in_stock"]
-    list_filter = [("type__name", named_filter(_("Product type"))), "in_stock"]
+    list_display = ["get_lot", "category", "get_type_name", "title", "price", "in_stock"]
+    list_filter = [("type__name", named_filter(_("Product type"))), "category", "in_stock"]
     search_fields = ["id", "guid", "title"]
     form = ProductForm
     fieldsets = (
-        (None, {"fields": ["type", "title", "description", "price", "in_stock"]}),
+        (None, {"fields": ["category", "type", "title", "description", "price", "in_stock"]}),
         ("Additional info", {"fields": ["guid", "created_at", "updated_at"]}),
     )
     readonly_fields = ["guid", "created_at", "updated_at"]
