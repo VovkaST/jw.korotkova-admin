a4b6fe6c6481878273cd056036ecb94a7bf4d9bb
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-05-03 12:14:00 +0300

    #40: Socials link added

---
+++ root/assets/src/components/icons/socials/index.ts
@@ -0,0 +1,6 @@
+import InstagramIcon from './IconInstagram.vue';
+import VKIcon from './IconVK.vue';
+import TelegramIcon from './IconTelegram.vue';
+import WhatsAppIcon from './IconWhatsapp.vue';
+
+export { InstagramIcon, TelegramIcon, VKIcon, WhatsAppIcon };
--- root/assets/src/views/MainView.vue
+++ root/assets/src/views/MainView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import WelcomeSection from '@/components/welcome/WelcomeSection.component.vue';
+
+import { InstagramIcon, VKIcon, TelegramIcon, WhatsAppIcon } from '@/components/icons/socials';
 </script>
 
 <template>
   <div class="welcome-screen d-flex flex-column justify-content-center gap-3">
-    <h1 class="welcome-screen__header text-center">Добро пожаловать!</h1>
+    <h1 class="welcome-screen__header text-center mb-4">Добро пожаловать!</h1>
     <router-link :to="{ name: 'jewelries' }">
       <WelcomeSection>
         <template #title><b>J</b>e<b>w</b>elries.Korotkova</template>
@@ -23,14 +25,72 @@ import WelcomeSection from '@/components/welcome/WelcomeSection.component.vue';
         <template #description>Ответы на вопросы и путь в новую жизнь</template>
       </WelcomeSection>
     </router-link>
-    <div class="welcome-screen__sign">
+    <div class="welcome-screen__sign mt-4">
       <span>Ваш мастер Наталья Короткова</span>
+    </div>
+    <div class="socials d-flex flex-row justify-content-center align-items-center mt-5">
+      <a href="https://www.instagram.com/jw.korotkova" target="_blank" class="social-link">
+        <InstagramIcon class="social-icon instagram" />
+      </a>
+      <a href="https://vk.com/id2518849" target="_blank" class="social-link">
+        <VKIcon class="social-icon vk" />
+      </a>
+      <a href="https://t.me/JW_Korotkova" target="_blank" class="social-link">
+        <TelegramIcon class="social-icon telegram" />
+      </a>
+      <a href="https://wa.me/79686281681" target="_blank" class="social-link">
+        <WhatsAppIcon class="social-icon whatsapp" />
+      </a>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 $hoverTransition: 0.3s ease-in-out;
+
+.socials {
+  height: 45px;
+
+  .social-link {
+    margin: 1rem;
+  }
+
+  .social-icon {
+    height: 35px;
+    fill: #939393;
+    transition:
+      fill $hoverTransition,
+      height $hoverTransition;
+
+    &:hover {
+      height: 45px;
+    }
+
+    &.instagram {
+      &:hover {
+        fill: #000100;
+      }
+    }
+
+    &.vk {
+      &:hover {
+        fill: #0077ff;
+      }
+    }
+
+    &.telegram {
+      &:hover {
+        fill: #2aabee;
+      }
+    }
+
+    &.whatsapp {
+      &:hover {
+        fill: #25d366;
+      }
+    }
+  }
+}
 
 .welcome-screen {
   margin: 0 auto;
