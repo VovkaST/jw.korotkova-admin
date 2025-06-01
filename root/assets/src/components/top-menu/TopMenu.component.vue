bd4b74efb7ffde5f6a989e7f2a364fe908947b66
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-06-01 18:23:45 +0300

    #40: Top menu styling

--- root/assets/src/components/top-menu/TopMenu.component.vue
+++ root/assets/src/components/top-menu/TopMenu.component.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
 export interface MenuItem {
   title: string;
   onClick: void;
@@ -8,30 +10,55 @@ defineProps<{
   title: string;
   items: Array<MenuItem>;
 }>();
+
+const titleEl = ref();
+const titleWidth = computed(() => {
+  return titleEl.value ? `${titleEl.value.offsetWidth}px` : null;
+});
 </script>
 
 <template>
-  <span class="menu-title">{{ title }}</span>
-  <nav>
-    <ul class="nav-menu d-flex flex-row justify-content-between align-items-center">
-      <li class="nav-menu__item" v-for="item in items">
-        <a @click.prevent="item.onClick" href="#">{{ item.title }}</a>
-      </li>
-    </ul>
-  </nav>
+  <div class="top-menu-container d-flex flex-row nowrap justify-content-between align-items-center">
+    <span class="menu-title" ref="titleEl">{{ title }}</span>
+    <nav>
+      <ul class="nav-menu m-0 p-0 d-flex flex-row">
+        <li class="nav-menu__item" v-for="item in items">
+          <a @click.prevent="item.onClick" href="#">{{ item.title }}</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="user-section" :style="{ width: titleWidth }"></div>
+  </div>
 </template>
 
 <style scoped lang="scss">
+$textColor: #6a6a6acc;
+$borderColor: #939393;
+
 .menu-title {
   font-family: var(--font-family-calligraphy);
   font-size: 2rem;
 }
 .nav-menu {
   list-style: none;
+  gap: 3rem;
 
   &__item {
-    display: block;
-    border: 1px solid red;
+    & a {
+      display: block;
+      font-weight: 400;
+      padding: 1rem 3rem;
+      transition:
+        color 0.3s,
+        border-bottom-color 0.3s;
+      color: $textColor;
+      border-bottom: 1px solid transparent;
+
+      &:hover {
+        color: hsl(from $textColor h s calc(l - 100));
+        border-bottom: 1px solid $borderColor;
+      }
+    }
   }
 }
 </style>
