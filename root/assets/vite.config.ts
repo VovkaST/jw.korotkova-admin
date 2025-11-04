6631b213cbeaafb12ae70106f7aee0df93c2cae6
Korotkov Vladimir <vovka.steel911@mail.ru>
2025-11-04 16:43:16 +0300

    #40. Add vite-plugin-html with other configuration

--- root/assets/vite.config.ts
+++ root/assets/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import { resolve } from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,42 @@ export default defineConfig({
     'process.env': {}, // Пустой объект для process.env
     'process': { env: {} }, // Полифил для process
   },
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    createHtmlPlugin({
+      minify: true,
+      /**
+       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+       * @default src/main.ts
+       */
+      entry: 'src/main.ts',
+      /**
+       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+       * @default index.html
+       */
+      // template: 'public/index.html',
+
+      /**
+       * Data that needs to be injected into the index.html ejs template
+       */
+      inject: {
+        data: {
+          siteTitle: 'index',
+        },
+        tags: [
+          {
+            injectTo: 'body-prepend',
+            tag: 'div',
+            attrs: {
+              id: 'tag',
+            },
+          },
+        ],
+      },
+    })
+  ],
   server: {
     host: '0.0.0.0',
     port: 8080,
